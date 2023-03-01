e621000a43296100f518123b978afe1a5367824f
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-03-01 17:42:40 +0000

    More mappings

--- deskdock-server/build.gradle.kts
+++ deskdock-server/build.gradle.kts
@@ -2,10 +2,15 @@ plugins {
     id("desk-dock-reverse-engineering-plugin")
 }
 
+repositories {
+    maven("https://maven.quiltmc.org/repository/release/")
+    maven("https://maven.quiltmc.org/repository/snapshot/")
+}
+
 dependencies {
     deskdock("com.floriandraschbacher:deskdock:1.3.1:server")
 
-    enigma("cuchaz:enigma-swing:2.3.0")
+    enigma("org.quiltmc:enigma-swing:1.7.0-SNAPSHOT")
     enigmaDep("net.fabricmc:name-proposal:0.1.4")
 }
 
