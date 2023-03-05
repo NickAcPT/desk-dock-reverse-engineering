e6210007490c58840c7037b27db855cd3487917a
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-03-05 17:26:04 +0000

    I don't know

---
+++ deskdock-enigma/src/main/kotlin/io/github/nickacpt/reverseengineering/deskdock/enigma/index/GetterSetterDeskDockIndexer.kt
@@ -0,0 +1,47 @@
+package io.github.nickacpt.reverseengineering.deskdock.enigma.index
+
+import io.github.nickacpt.reverseengineering.deskdock.enigma.index.model.IndexEntryKey
+import org.benf.cfr.reader.bytecode.analysis.parse.Expression
+import org.benf.cfr.reader.bytecode.analysis.parse.StatementContainer
+import org.benf.cfr.reader.bytecode.analysis.parse.rewriters.AbstractExpressionRewriter
+import org.benf.cfr.reader.bytecode.analysis.parse.rewriters.ExpressionRewriterFlags
+import org.benf.cfr.reader.bytecode.analysis.parse.utils.SSAIdentifiers
+import org.benf.cfr.reader.bytecode.analysis.parse.wildcard.Wildcard
+import org.benf.cfr.reader.bytecode.analysis.parse.wildcard.WildcardMatch
+import org.benf.cfr.reader.entities.ClassFile
+import org.benf.cfr.reader.entities.Method
+import org.objectweb.asm.tree.ClassNode
+import org.objectweb.asm.tree.MethodNode
+
+class MethodPatternDeskDockIndexer<T : Any>(val patterns: WildcardMatch.() -> List<Wildcard<out Expression>>) : AbstractDeskDockIndexer<T>() {
+
+    inner class ExpressionVisitor() : AbstractExpressionRewriter() {
+        override fun rewriteExpression(expression: Expression?, ssaIdentifiers: SSAIdentifiers<*>?, statementContainer: StatementContainer<*>?, flags: ExpressionRewriterFlags?): Expression {
+            return super.rewriteExpression(expression, ssaIdentifiers, statementContainer, flags)
+        }
+    }
+
+    override fun indexMethod(clazz: ClassNode,
+                             method: MethodNode,
+                             cfrClazz: ClassFile,
+                             cfrMethod: Method,
+                             ownerEntry: IndexEntryKey.ClassIndexEntry,
+                             methodEntry: IndexEntryKey.MethodIndexEntry): Map<IndexEntryKey, T>? {
+        val wildcardMatch = WildcardMatch()
+
+        return super.indexMethod(clazz, method, cfrClazz, cfrMethod, ownerEntry, methodEntry)
+    }
+}
+
+class GetterSetterDeskDockIndexer : AbstractDeskDockIndexer<GetterSetterDeskDockIndexer.MethodTypeData>() {
+    data class MethodTypeData(val field: String, val isGetter: Boolean)
+
+    override fun indexMethod(clazz: ClassNode,
+                             method: MethodNode,
+                             cfrClazz: ClassFile,
+                             cfrMethod: Method,
+                             ownerEntry: IndexEntryKey.ClassIndexEntry,
+                             methodEntry: IndexEntryKey.MethodIndexEntry): Map<IndexEntryKey, MethodTypeData>? {
+        return super.indexMethod(clazz, method, cfrClazz, cfrMethod, ownerEntry, methodEntry)
+    }
+}
--- deskdock-enigma/src/main/kotlin/io/github/nickacpt/reverseengineering/deskdock/enigma/services/DeskDockJarIndexerService.kt
+++ deskdock-enigma/src/main/kotlin/io/github/nickacpt/reverseengineering/deskdock/enigma/services/DeskDockJarIndexerService.kt
@@ -4,15 +4,23 @@ import cuchaz.enigma.analysis.index.JarIndex
 import cuchaz.enigma.api.service.JarIndexerService
 import cuchaz.enigma.classprovider.ClassProvider
 import io.github.nickacpt.reverseengineering.deskdock.enigma.index.AbstractDeskDockIndexer
+import io.github.nickacpt.reverseengineering.deskdock.enigma.index.GetterSetterDeskDockIndexer
 import io.github.nickacpt.reverseengineering.deskdock.enigma.index.model.IndexEntryKey
 import io.github.nickacpt.reverseengineering.deskdock.enigma.utils.cfr.ClassNodeViewCfrClassSource
+import org.benf.cfr.reader.bytecode.analysis.loc.BytecodeLoc
+import org.benf.cfr.reader.bytecode.analysis.opgraph.op4rewriters.util.MiscStatementTools
+import org.benf.cfr.reader.bytecode.analysis.parse.expression.LValueExpression
+import org.benf.cfr.reader.bytecode.analysis.parse.wildcard.WildcardMatch
+import org.benf.cfr.reader.bytecode.analysis.structured.statement.StructuredReturn
 import org.benf.cfr.reader.state.DCCommonState
 import org.benf.cfr.reader.util.getopt.OptionsImpl
 
 object DeskDockJarIndexerService : JarIndexerService {
     private val cfrOptions = OptionsImpl(emptyMap())
 
-    private val indexers = listOf<AbstractDeskDockIndexer<*>>()
+    private val indexers = listOf<AbstractDeskDockIndexer<*>>(
+            GetterSetterDeskDockIndexer()
+    )
 
     private val gigaIndexResult = mutableMapOf<IndexEntryKey, MutableList<Any>>()
 
@@ -42,8 +50,24 @@ object DeskDockJarIndexerService : JarIndexerService {
                 // We have to index the class
                 results += indexer.indexClass(clazzNode, cfrClazz)
 
+                if (clazzNode.name == "com/floriandraschbacher/deskdock/server/class_65") {
+                    println()
+                }
+
                 // Now index the methods
                 results += cfrClazz.methods.map { cfrMethod ->
+
+                    if (cfrMethod.name == "method_160") {
+                        val w = WildcardMatch()
+
+                        val expr = w.getLValueWildCard("returnField")
+                        val ret = StructuredReturn(BytecodeLoc.NONE, LValueExpression(expr), null)
+
+                        MiscStatementTools.linearise(cfrMethod.analysis).map {
+                            w.match(ret, it)
+                        }
+                    }
+
                     val methodNode = clazzNode.methods.first { it.name == cfrMethod.name && cfrMethod.methodPrototype.originalDescriptor == it.desc }
                     val methodIndexEntry = IndexEntryKey.MethodIndexEntry(clazzIndexEntry, cfrMethod.name, cfrMethod.methodPrototype.originalDescriptor)
 
