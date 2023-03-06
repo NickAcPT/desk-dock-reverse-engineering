e6210000e01f566a1a581efc2555ada68fe3a18c
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-03-06 21:55:33 +0000

    Automate Synergy packets

--- buildSrc/src/main/kotlin/io/github/nickacpt/reverseengineering/deskdock/plugin/tasks/AutomateMappingsTask.kt
+++ buildSrc/src/main/kotlin/io/github/nickacpt/reverseengineering/deskdock/plugin/tasks/AutomateMappingsTask.kt
@@ -1,10 +1,36 @@
 package io.github.nickacpt.reverseengineering.deskdock.plugin.tasks
 
+import io.github.nickacpt.reverseengineering.deskdock.plugin.providers.IntermediaryDeskDockProvider
+import io.github.nickacpt.reverseengineering.deskdock.plugin.utils.mappings.AsmUtils
+import io.github.nickacpt.reverseengineering.deskdock.plugin.utils.mappings.MappingUtils
+import io.github.nickacpt.reverseengineering.deskdock.plugin.utils.workspace
+import net.fabricmc.mappingio.MappedElementKind
+import net.fabricmc.mappingio.MappingWriter
+import net.fabricmc.mappingio.format.MappingFormat
 import org.gradle.api.tasks.TaskAction
 
 abstract class AutomateMappingsTask : DeskDockTaskBase() {
     @TaskAction
     fun automate() {
+        val mappingsDir = project.workspace.type.getMappingsDirectory(project)
+        val intermediaryNodes = AsmUtils.viewJarAsNodes(IntermediaryDeskDockProvider.provide(project))
+
+        val mappings = MappingUtils.loadMappings(mappingsDir)
+
+        intermediaryNodes.forEach { (_, node) ->
+            if (node.superName == "com/floriandraschbacher/deskdock/server/class_39") {
+                mappings.visitClass(node.name)
+
+                val name =
+                    "com/floriandraschbacher/deskdock/server/logic/synergy/packets/" + node.name.removePrefix("com/floriandraschbacher/deskdock/server/")
+
+                mappings.visitDstName(MappedElementKind.CLASS, 0, name)
+            }
+        }
+
+        MappingWriter.create(mappingsDir, MappingFormat.ENIGMA).use {
+            mappings.accept(it)
+        }
     }
 
 }
