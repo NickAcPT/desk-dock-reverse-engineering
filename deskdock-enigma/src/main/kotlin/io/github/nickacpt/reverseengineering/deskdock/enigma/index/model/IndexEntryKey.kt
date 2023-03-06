e621000a611806d05515cc30a0b00831d14cd276
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-03-06 21:38:34 +0000

    Auto mapping based on simple method call

--- deskdock-enigma/src/main/kotlin/io/github/nickacpt/reverseengineering/deskdock/enigma/index/model/IndexEntryKey.kt
+++ deskdock-enigma/src/main/kotlin/io/github/nickacpt/reverseengineering/deskdock/enigma/index/model/IndexEntryKey.kt
@@ -21,6 +21,23 @@ sealed interface IndexEntryKey {
             return result
         }
     }
-    data class FieldIndexEntry(val owner: ClassIndexEntry, override val name: String, val desc: String) : IndexEntryKey
+
+    data class FieldIndexEntry(val owner: ClassIndexEntry, override val name: String, val desc: String) : IndexEntryKey {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as FieldIndexEntry
+
+            if (name != other.name) return false
+            return desc == other.desc
+        }
+
+        override fun hashCode(): Int {
+            var result = name.hashCode()
+            result = 31 * result + desc.hashCode()
+            return result
+        }
+    }
     data class MethodParameterIndexEntry(val method: MethodIndexEntry, override val name: String) : IndexEntryKey
 }
